85642ac53ba804b025acb9905469beb6b0fdebfb
eclipse <zdanevych.y@gmail.com>
2024-01-17 03:55:17 +0200

    Initial commit with initial dark theme settings

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import Layout from "@/Layouts/Layout.vue";
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+});
+</script>
+
+<template>
+    <Layout>
+
+    </Layout>
+</template>
+
